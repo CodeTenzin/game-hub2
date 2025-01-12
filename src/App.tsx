7688a4ed14298ae55f07f259386707f0be300ab4
Tenzin Wangyal <tenz_wang333@yahoo.com>
2025-01-12 16:25:53 -0500

    Built a responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <>
-      <Button>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg="coral">
+          Nav
+        </GridItem>
+        <GridItem
+          area={"aside"}
+          bg="gold"
+          display={{ base: "none", lg: "block" }}
+        >
+          Aside
+        </GridItem>
+        {/* <Show when={{ base: false, lg: true }}>
+          <GridItem area={"aside"} bg="gold">
+            Aside
+          </GridItem>
+        </Show> */}
+        <GridItem area={"main"} bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 };
