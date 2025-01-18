a1e654e937a43ccbe10b3aa3cae68adea8234731
Tenzin Wangyal <tenz_wang333@yahoo.com>
2025-01-17 22:23:52 -0500

    Displaying platform icons.

---
+++ src/components/PlatforIconList.tsx
@@ -0,0 +1,41 @@
+import { Platform } from "@/hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatforIconList = ({ platforms }: Props) => {
+  const iconsMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconsMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatforIconList;
