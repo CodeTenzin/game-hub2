399f2a06fe45e77d0f25d85a0435cd584d0e6b97
Tenzin Wangyal <tenz_wang333@yahoo.com>
2025-01-19 13:20:49 -0500

    Build search input.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
 };
 
 export default NavBar;
-
-//  b519e0d0eec44a6da3b277ce7e6a86fd
