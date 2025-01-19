e335002fc6b2f734ca42e2d65a5c9a3a6e8694fd
Tenzin Wangyal <tenz_wang333@yahoo.com>
2025-01-18 19:10:47 -0500

    Build platform selector.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatforIconList from "./components/PlatforIconList";
+import PlatformSelector from "./components/PlatformSelector";
 
 const App = () => {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -33,6 +35,7 @@ const App = () => {
           />
         </GridItem>
         <GridItem area={"main"}>
+          <PlatformSelector />
           <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
